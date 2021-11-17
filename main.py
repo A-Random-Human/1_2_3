dce63197ff3eb7d7213ff362af9dd7df7122d921
Haley Groesbeck <70415062+A-Random-Human@users.noreply.github.com>
2021-11-17 21:08:10 +0000

    disappearing apple

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) 
 wn.bgpic("background.gif")
+wn.tracer(True)
 
 drawer = trtl.Turtle()
 drawer.penup()
@@ -24,7 +25,7 @@ def draw_apple(active_apple):
   wn.update()
 
 def update_apple():
-  apple.goto(apple.xcor(),-200) 
+  apple.goto(apple.xcor(),-250) 
 
 #-----function calls-----
 draw_apple(apple)
@@ -37,9 +38,14 @@ def draw_an_A():
   drawer.color("white")
   drawer.write("A", font=("Arial", 74, "bold")) 
 
+def clear_screen():
+  apple.hideturtle()
+  drawer.clear()
+
 def on_drop():
   draw_an_A()
   update_apple()
+  clear_screen()
 
 wn.onkeypress(on_drop, "a")
 
